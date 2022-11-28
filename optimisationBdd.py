db3e2a186648a8bb56c4bd8ab91503d4c059b9ef
morgann.riu <morgann.riu@imerir.com>
2022-11-28 21:41:21 +0100

    norme 3nf

--- optimisationBdd.py
+++ optimisationBdd.py
@@ -18,30 +18,30 @@ def connectBase():
 def normaliseBase():
     ''' normalise la base de données en utilisant la norme 3NF
     '''
-    # créer une table villes2 avec les champs id, insee_code, zip_code, name, slug, gps_lat, gps_lng et department_id
+    # créer une table villes2 avec les champs id, insee_code, zip_code, name, slug, gps_lat, gps_lng et department_id (foreign key) si elle n'existe pas
     conn = connectBase()
     c = conn.cursor()
-    c.execute('CREATE TABLE villes2 (insee_code INTEGER PRIMARY KEY, zip_code int, name TEXT, slug TEXT, gps_lat TEXT, gps_lng TEXT, department_id INTEGER)')
+    c.execute('CREATE TABLE IF NOT EXISTS villes2 (id INTEGER PRIMARY KEY, insee_code TEXT, zip_code TEXT, name TEXT, slug TEXT, gps_lat TEXT, gps_lng TEXT, department_id INTEGER, FOREIGN KEY(department_id) REFERENCES departments(id))')
     conn.commit()
 
-    # créer une table departments avec les champs id, code, name, slug et region_id
-    c.execute('CREATE TABLE departments (code INTEGER PRIMARY KEY, name TEXT, slug TEXT, region_id INTEGER)')
+    # créer une table departments avec les champs id, code, name, slug et region_id (foreign key) si elle n'existe pas
+    c.execute('CREATE TABLE IF NOT EXISTS departments (id INTEGER PRIMARY KEY, code TEXT, name TEXT, slug TEXT, region_id INTEGER, FOREIGN KEY(region_id) REFERENCES regions(id))')
     conn.commit()
 
-    # créer une table regions avec les champs id, code, name, slug
-    c.execute('CREATE TABLE regions (code INTEGER PRIMARY KEY, name TEXT, slug TEXT)')
+    # créer une table regions avec les champs id, code, name, slug si elle n'existe pas
+    c.execute('CREATE TABLE IF NOT EXISTS regions (id INTEGER PRIMARY KEY, code TEXT, name TEXT, slug TEXT)')
     conn.commit()
 
-    # créer une table villes_departments avec les champs id, ville_id et department_id
-    c.execute('CREATE TABLE villes_departments (id INTEGER PRIMARY KEY, ville_id INTEGER, department_id INTEGER)')
+    # créer une table villes_departments avec les champs id, ville_id (foreign key) et department_id (foreign keys) si elle n'existe pas
+    c.execute('CREATE TABLE IF NOT EXISTS villes_departments (id INTEGER PRIMARY KEY, ville_id INTEGER, department_id INTEGER, FOREIGN KEY(ville_id) REFERENCES villes2(id), FOREIGN KEY(department_id) REFERENCES departments(id))')
     conn.commit()
 
-    # créer une table villes_regions avec les champs id, ville_id et region_id
-    c.execute('CREATE TABLE villes_regions (id INTEGER PRIMARY KEY, ville_id INTEGER, region_id INTEGER)')
+    # créer une table villes_regions avec les champs id, ville_id (foreign key) et region_id (foreign key) si elle n'existe pas
+    c.execute('CREATE TABLE IF NOT EXISTS villes_regions (id INTEGER PRIMARY KEY, ville_id INTEGER, region_id INTEGER, FOREIGN KEY(ville_id) REFERENCES villes2(id), FOREIGN KEY(region_id) REFERENCES regions(id))')
     conn.commit()
 
-    # créer une table villes_departments_regions avec les champs id, ville_id, department_id et region_id
-    c.execute('CREATE TABLE villes_departments_regions (id INTEGER PRIMARY KEY, ville_id INTEGER, department_id INTEGER, region_id INTEGER)')
+    # créer une table villes_departments_regions avec les champs id, ville_id (foreign key), department_id (foreign key) et region_id (foreign key) si elle n'existe pas
+    c.execute('CREATE TABLE IF NOT EXISTS villes_departments_regions (id INTEGER PRIMARY KEY, ville_id INTEGER, department_id INTEGER, region_id INTEGER, FOREIGN KEY(ville_id) REFERENCES villes2(id), FOREIGN KEY(department_id) REFERENCES departments(id), FOREIGN KEY(region_id) REFERENCES regions(id))')
     conn.commit()
 
     # remplir les tables
@@ -49,36 +49,56 @@ def normaliseBase():
     villes = c.fetchall()
 
     for ville in villes:
+        codeInsee = ville[1]
+        zip_code = ville[2]
+        name = ville[3]
+        slug = ville[4]
+        gps_lat = ville[5]
+        gps_lng = ville[6]
+        department_code = ville[7]
+        department_name = ville[8]
+        department_slug = ville[9]
+        region_code = ville[10]
+        region_name = ville[11]
+        region_slug = ville[12]
+        # verifie si la region existe
+        c.execute('SELECT * FROM regions WHERE code = ?', (region_code,))
+        region = c.fetchone()
+        region_id = region[0]
+        if region == None:
+            c.execute('INSERT INTO regions (code, name, slug) VALUES (?, ?, ?)', (region_code, region_name, region_slug))
+            conn.commit()
+            region_id = c.lastrowid
+        # verifie si le departement existe
+        c.execute('SELECT * FROM departments WHERE code = ?', (department_code,))
+        department = c.fetchone()
+        department_id = department[0]
+        if department == None:
+            c.execute('INSERT INTO departments (code, name, slug, region_id) VALUES (?, ?, ?, ?)', (department_code, department_name, department_slug, region_id))
+            conn.commit()
+            department_id = c.lastrowid
         # remplir la table villes2
-        c.execute('INSERT INTO villes2 (insee_code, zip_code, name, slug, gps_lat, gps_lng, department_id) VALUES (?, ?, ?, ?, ?, ?, ?)', (ville[1], ville[2], ville[3], ville[4], ville[5], ville[6], ville[7]))
-        conn.commit()
-        # remplir la table departments
-        c.execute('INSERT INTO departments (code, name, slug, region_id) VALUES (?, ?, ?, ?)', (ville[7], ville[8], ville[9], ville[10]))
-        conn.commit()
-        # remplir la table regions
-        c.execute('INSERT INTO regions (code, name, slug) VALUES (?, ?, ?)', (ville[10], ville[11], ville[12]))
-        conn.commit()
+        c.execute('INSERT INTO villes2 (insee_code, zip_code, name, slug, gps_lat, gps_lng, department_id) VALUES (?, ?, ?, ?, ?, ?, ?)', (codeInsee, zip_code, name, slug, gps_lat, gps_lng, department_id))
         # remplir la table villes_departments
-        c.execute('INSERT INTO villes_departments (ville_id, department_id) VALUES (?, ?)', (ville[0], ville[7]))
+        ville_id = c.lastrowid
+        c.execute('INSERT INTO villes_departments (ville_id, department_id) VALUES (?, ?)', (ville_id, department[0]))
+        ville2 = c.fetchone()
+        # remplir la table villes_regions
+        c.execute('INSERT INTO villes_regions (ville_id, region_id) VALUES (?, ?)', (ville_id, region[0]))
         conn.commit()
         # remplir la table villes_regions
-        c.execute('INSERT INTO villes_regions (ville_id, region_id) VALUES (?, ?)', (ville[0], ville[8]))
+        c.execute('INSERT INTO villes_departments_regions (ville_id, department_id, region_id) VALUES (?, ?, ?)', (ville_id, department_id, region_id))
         conn.commit()
         # remplir la table villes_departments_regions
-        c.execute('INSERT INTO villes_departments_regions (ville_id, department_id, region_id) VALUES (?, ?, ?)', (ville[0], ville[7], ville[8]))
+        c.execute('INSERT INTO villes_departments_regions (ville_id, department_id, region_id) VALUES (?, ?, ?)', (ville_id, department_id, region_id))
         conn.commit()
-
-
-
-
     # supprimer la table villes
     c.execute('DROP TABLE villes')
     conn.commit()
-
     # renommer la table villes2 en villes
     c.execute('ALTER TABLE villes2 RENAME TO villes')
     conn.commit()
-
 # main
 if __name__ == '__main__':
     normaliseBase()
+    print('Base de données normalisée')
